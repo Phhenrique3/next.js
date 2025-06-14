34d146a17d24878c61f100bcee446a0afed3b7fc
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-06-14 13:53:43 -0300

    Theme StyleGlobal

--- pages/faq.js
+++ pages/faq.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function FAQPage() {
   return (
     <div>
-      <h1>teste</h1>
+      <h1>teste  </h1>
       <Link href="/">ir para FAQ</Link>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import Link from "next/link";
+import React from "react";
 
+function Title({ children, as }) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>{children}</Tag>
+      <style jsx>
+        {`
+          ${Tag} {
+            color: red;
+            font-family: sans-serif;
+          }
+        `}
+      </style>
+    </React.Fragment>
+  );
+}
 
 export default function Homepage() {
   return (
     <div>
-      <h1>teste</h1>
-      <Link href="/faq" >
-        Ir para pagina de FAQ
-      </Link>
+      <Title as="h2">Alura Case Home </Title>
+      <Link href="/faq">Ir para pagina de FAQ</Link>
     </div>
   );
 }
